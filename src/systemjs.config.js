630b84b707405d7f6667c00296a47ab9208e1e60
Haylee Jane Monteiro <haylee,jane3@gmail.com>
2017-04-29 11:08:33 -0600

    change base url

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -8,6 +8,7 @@
       // paths serve as alias
       'npm:': 'node_modules/'
     },
+    baseUrl: '/hayleemonteiro/'
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
