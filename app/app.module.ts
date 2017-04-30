92797ad2676b78ce74635b31f8042e4c9ba672b7
Haylee Jane Monteiro <haylee,jane3@gmail.com>
2017-04-29 23:42:51 -0600

    fix 404s

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router'
-import {HttpModule} from '@angular/http'
+import { HttpModule } from '@angular/http'
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component'
 import { HomeComponent } from './home/home.component'
 import { InvolvementComponent } from './involvement/involvement.component'
